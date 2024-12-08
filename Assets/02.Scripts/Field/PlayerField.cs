4009d05353fee450d7badb5fd3cd7677de74966d
yachung <dbstjd8267@ajou.ac.kr>
2024-12-09 01:08:37 +0900

    PlayerField 코드 정리

--- Assets/02.Scripts/Field/PlayerField.cs
+++ Assets/02.Scripts/Field/PlayerField.cs
@@ -7,15 +7,13 @@ using VContainer;
 
 public class PlayerField : NetworkBehaviour
 {
+    private readonly Vector2 hexSize = new Vector2(13f, 15f);
+
     [Inject] private readonly GameStateManager gameStateManager;
+    private BattleController battleController;
 
     [SerializeField] private Transform gridStartingPoint;
-
-    public Tile[,] Tiles = new Tile[8, 10];
-    [Networked, OnChangedRender(nameof(IsBattleChanged))] public bool IsBattle { get; set; }
-
-    public Action<bool> OnIsBattleChanged;
-
+    private Tile[,] Tiles = new Tile[8, 10];
     private Tile this[Vector2Int coord, bool isDrag = false]
     {
         get
@@ -40,31 +38,24 @@ public class PlayerField : NetworkBehaviour
             return null;
         }
     }
+    private Vector3 tileGridOffset;
 
-    public Tile GetBattleTile(Vector2Int coord) => this[coord];
-    public Tile GetDragTile(Vector2Int coord) => this[coord, isDrag: true];
-
+    [Networked, OnChangedRender(nameof(IsBattleChanged))] public bool IsBattle { get; set; }
     [Networked, Capacity(40)] public NetworkLinkedList<Champion> Champions => default;
+    public Action<bool> OnIsBattleChanged;
 
     public Pose cameraPose;
     public Pose reverseCameraPose;
-    public Transform refTransform;
-    public Vector3 spawnPosition;
 
-    //private ChampionDragAndDrop championDragHandler;
-    //private BattleController battleController;
-    private Vector3 gridOffset;
-    private BattleController battleController;
-    private Vector2 hexSize = new Vector2(13f, 15f);
+
+    #region 초기화 및 설정
 
     private void Awake()
     {
-        refTransform = transform;
-
         cameraPose = new Pose(Camera.main.transform.position + transform.position, Camera.main.transform.rotation);
         reverseCameraPose = new Pose(cameraPose.position + (new Vector3(0, 0, 120)), Quaternion.Euler(0, 180, 0) * cameraPose.rotation);
 
-        gridOffset = gridStartingPoint.position;
+        tileGridOffset = gridStartingPoint.position;
 
         battleController = GetComponentInChildren<BattleController>();
         battleController.playerField = this;
@@ -95,59 +86,19 @@ public class PlayerField : NetworkBehaviour
         }
     }
 
-    public void IsBattleChanged()
-    {
-        OnIsBattleChanged?.Invoke(IsBattle);
-    }
+    #endregion
 
-    public void StartBattle()
-    {
-        IsBattle = true;
+    #region 타일 관련 메서드
 
-        // 전투 시작 시 BattleController에서 전투를 시작하도록 호출
-        battleController.StartBattle();
-    }
+    /// <summary>
+    /// 전투 타일 반환
+    /// </summary>
+    public Tile GetBattleTile(Vector2Int coord) => this[coord];
 
-    public void BattleEnd()
-    {
-        IsBattle = false;
-
-        foreach (var tile in Tiles)
-            tile.RemoveChampion();
-
-        battleController.BattleEnd();
-
-        foreach (var champion in Champions)
-            champion.IsAwayTeam = false;
-    }
-
-    public void SpawnChampion(Vector2Int Coord, Champion champion)
-    {
-        this[Coord].DeployChampion(champion);
-    }
-
-    public void BattleInitializeForEnemy(List<Tile> championTiles)
-    {
-        foreach (var tile in championTiles)
-        {
-            Vector2Int target = new Vector2Int(Tiles.GetLength(0) - tile.Coordinate.x - 1, Tiles.GetLength(1) - tile.Coordinate.y - 1);
-
-            if (tile.Champion != null)
-            {
-                tile.Champion.IsAwayTeam = true;
-                this[target].DeployChampion(tile.Champion, true);
-            }
-        }
-    }
-
-    public void ChampionRespawn()
-    {
-        foreach (var champion in Champions)
-        {
-            champion.Respawn();
-            SpawnChampion(champion.ReadyCoord, champion);
-        }
-    }
+    /// <summary>
+    /// 드래그 가능한 타일 반환
+    /// </summary>
+    public Tile GetDragTile(Vector2Int coord) => this[coord, isDrag: true];
 
     /// <summary>
     /// TestCode
@@ -169,6 +120,9 @@ public class PlayerField : NetworkBehaviour
         return result;
     }
 
+    /// <summary>
+    /// 대기 필드에서 빈 타일 반환
+    /// </summary>
     public Tile GetEmptyWaitField()
     {
         for (int x = 0; x < 8; x++) // WaitField는 x 좌표가 0부터 7까지 범위에 존재
@@ -181,6 +135,61 @@ public class PlayerField : NetworkBehaviour
 
         // 빈 필드가 없는 경우 null 반환
         return null;
+    }
+
+    #endregion
+
+    #region 챔피언 배치 및 관리
+
+
+    /// <summary>
+    /// 챔피언을 타일에 배치 및 이동
+    /// </summary>
+    public void SetChampion(Vector2Int origin, Vector2Int target, Champion selectChampion, Action<Vector2Int, Vector2Int> deployAction)
+    {
+        Debug.Log($"origin : {origin}, target : {target}");
+
+        if (!IsValidCoordinate(target))
+        {
+            Tiles[origin.x, origin.y].DeployChampion(selectChampion, deployAction);
+            return;
+        }
+
+        if (Tiles[target.x, target.y].IsOccupied(out Champion placedChampion))
+        {
+            Tiles[origin.x, origin.y].DeployChampion(placedChampion, deployAction);
+            Tiles[target.x, target.y].DeployChampion(selectChampion, deployAction);
+        }
+        else
+        {
+            Tiles[target.x, target.y].DeployChampion(selectChampion, deployAction);
+            Tiles[origin.x, origin.y].RemoveChampion();
+        }
+    }
+
+    public void SetChampion(Vector3 origin, Vector3 target, Champion selectChampion, Action<Vector2Int, Vector2Int> deployAction)
+    {
+        SetChampion(CalculateCoordinate(origin), CalculateCoordinate(target), selectChampion, deployAction);
+    }
+
+    /// <summary>
+    /// 챔피언 생성
+    /// </summary>
+    public void SpawnChampion(Vector2Int Coord, Champion champion)
+    {
+        this[Coord].DeployChampion(champion);
+    }
+
+    /// <summary>
+    /// 내가 보유한 챔피언들 내 필드의 ReadyCoord로 재소환
+    /// </summary>
+    public void ChampionRespawn()
+    {
+        foreach (var champion in Champions)
+        {
+            champion.Respawn();
+            SpawnChampion(champion.ReadyCoord, champion);
+        }
     }
 
     /// <summary>
@@ -209,52 +218,10 @@ public class PlayerField : NetworkBehaviour
             return false;
     }
 
-    /// <summary>
-    /// target이 유효한 위치가 아니면 selectChampion을 다시 origin 위치로
-    /// target에 champion이 있다면 origin 위치로 보내고 selectChampion을 target으로 보냄
-    /// target이 비어있다면 selectChampion을 target위치로 보냄.
-    /// origin
-    /// </summary>
-    /// <param name="origin"></param>
-    /// <param name="target"></param>
-    /// <param name="selectChampion"></param>
-    public void SetChampion(Vector2Int origin, Vector2Int target, Champion selectChampion, Action<Vector2Int, Vector2Int> deployAction)
-    {
-        Debug.Log($"origin : {origin}, target : {target}");
-
-        if (!IsValidCoordinate(target))
-        {
-            Tiles[origin.x, origin.y].DeployChampion(selectChampion, deployAction);
-            return;
-        }
-
-        if (Tiles[target.x, target.y].IsOccupied(out Champion placedChampion))
-        {
-            Tiles[origin.x, origin.y].DeployChampion(placedChampion, deployAction);
-            Tiles[target.x, target.y].DeployChampion(selectChampion, deployAction);
-        }
-        else
-        {
-            Tiles[target.x, target.y].DeployChampion(selectChampion, deployAction);
-            Tiles[origin.x, origin.y].RemoveChampion();
-        }
-    }
-
-    public void SetChampion(Vector3 origin, Vector3 target, Champion selectChampion, Action<Vector2Int, Vector2Int> deployAction)
-    {
-        SetChampion(CalculateCoordinate(origin), CalculateCoordinate(target), selectChampion, deployAction);
-    }
-
-    [Rpc(RpcSources.StateAuthority, RpcTargets.InputAuthority)]
-    public void RPC_SetChampion(Vector2Int coord, Champion champion)
-    {
-        Tiles[coord.x, coord.y].Champion = champion;
-    }
-
     public void UpdatePositionOnHost(Vector3 origin, Vector3 target)
     {
         RPC_UpdatePositionOnHost(CalculateCoordinate(origin), CalculateCoordinate(target));
-    }    
+    }
 
     [Rpc(sources: RpcSources.All, targets: RpcTargets.StateAuthority)]
     public void RPC_UpdatePositionOnHost(Vector2Int source, Vector2Int target)
@@ -284,30 +251,59 @@ public class PlayerField : NetworkBehaviour
         }
     }
 
-    [Rpc(sources: RpcSources.All, targets: RpcTargets.StateAuthority)]
-    public void RPC_UpdatePositionOnHost(Champion champion, Vector2Int target)
-    {
-        if (champion == null)
-            return;
+    #endregion
 
-        champion.ReadyCoord = target;
-        this[target].DeployChampion(champion);
+    #region 전투 시작 및 종료
+    public void IsBattleChanged()
+    {
+        OnIsBattleChanged?.Invoke(IsBattle);
     }
 
     /// <summary>
-    /// 입력된 좌표가 PlayerField에 유효한 좌표인지 확인
+    /// 전투 시작 시 초기화 작업
     /// </summary>
-    /// <param name="coordinate"></param>
-    /// <returns></returns>
-    private bool IsValidCoordinate(Vector2Int coordinate)
+    public void StartBattle()
     {
-        if (coordinate.x >= 0 && coordinate.x <= 8 && coordinate.y >= 0 && coordinate.y <= 9)
-            return true;
-        else
-            return false;
+        IsBattle = true;
+
+        // 전투 시작 시 BattleController에서 전투를 시작하도록 호출
+        battleController.StartBattle();
     }
 
-    #region Coordinate Calculate
+    public void BattleInitializeForEnemy(List<Tile> championTiles)
+    {
+        foreach (var tile in championTiles)
+        {
+            Vector2Int target = new Vector2Int(Tiles.GetLength(0) - tile.Coordinate.x - 1, Tiles.GetLength(1) - tile.Coordinate.y - 1);
+
+            if (tile.Champion != null)
+            {
+                tile.Champion.IsAwayTeam = true;
+                this[target].DeployChampion(tile.Champion, true);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 전투 종료시 초기화 작업
+    /// </summary>
+    public void BattleEnd()
+    {
+        IsBattle = false;
+
+        foreach (var tile in Tiles)
+            tile.RemoveChampion();
+
+        battleController.BattleEnd();
+
+        foreach (var champion in Champions)
+            champion.IsAwayTeam = false;
+    }
+
+    #endregion
+
+    #region 좌표 계산
+
     public Vector2Int CalculateCoordinate(Vector3 inputPosition)
     {
         return GetHexCoordinate(inputPosition);
@@ -319,7 +315,7 @@ public class PlayerField : NetworkBehaviour
         float width = hexSize.x;
         float height = hexSize.y * 0.75f;  // 세로 간격은 육각형이 겹치는 정도를 반영한 값
 
-        Vector3 adjustedPosition = worldPosition - (Vector3)gridOffset;
+        Vector3 adjustedPosition = worldPosition - (Vector3)tileGridOffset;
 
         // 대략적인 타일 좌표 추정
         int approxY = Mathf.RoundToInt(adjustedPosition.z / height);
@@ -342,10 +338,24 @@ public class PlayerField : NetworkBehaviour
         float height = hexSize.y * 0.75f;  // 세로 간격
 
         float offsetX = (coordinate.y % 2 == 0) ? 0 : width / 2;  // 짝수 행 오프셋 계산
-        float posX = coordinate.x * width + offsetX + gridOffset.x;
-        float posY = coordinate.y * height + gridOffset.z;
+        float posX = coordinate.x * width + offsetX + tileGridOffset.x;
+        float posY = coordinate.y * height + tileGridOffset.z;
 
         return new Vector3(posX, 0, posY);  // 월드 좌표로 변환된 타일 위치
     }
+
+    /// <summary>
+    /// 입력된 좌표가 PlayerField에 유효한 좌표인지 확인
+    /// </summary>
+    /// <param name="coordinate"></param>
+    /// <returns></returns>
+    private bool IsValidCoordinate(Vector2Int coordinate)
+    {
+        if (coordinate.x >= 0 && coordinate.x <= 8 && coordinate.y >= 0 && coordinate.y <= 9)
+            return true;
+        else
+            return false;
+    }
+
     #endregion
 }
